dc86516a440a565996309f7ac2e43e695f9a68df
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-27 23:21:52 +0300

    added the image class in the database

--- gallery/pictures/models.py
+++ gallery/pictures/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'articles/')
+    name = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    
