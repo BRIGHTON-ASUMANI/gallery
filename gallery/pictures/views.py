497f28a507657db53d56ae689d7e8857c5ce8df5
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-09-27 16:27:17 +0300

    created the navbar and all the required templates

--- gallery/pictures/views.py
+++ gallery/pictures/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import HttpResponse,render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
 
 # Create your views here.
 def picture(request):
-    return render(request, picture.html')
+    return HttpResponse(request, 'picture.html')
+def picture_today(request):
+    date = dt.date.today()
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    return render(request, 'all-images/today-images.html', {"date": date,})
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
+def past_days_pictures(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(picture_today)
+
+    return render(request, 'all-images/past-images.html', {"date": date})
