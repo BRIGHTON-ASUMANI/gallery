ecb5cafd13723010c030bf8c01e7550fad15f393
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-01 02:03:45 +0300

    added the ImageAdmin to represent the horizontal mtm relationship

--- gallery/pictures/admin.py
+++ gallery/pictures/admin.py
@@ -4,6 +4,6 @@ from .models import Image, Category, Location
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal =('category',)
 
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
 admin.site.register(Category)
 admin.site.register(Location)
