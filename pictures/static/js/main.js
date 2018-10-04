7accd83bd407dd10570d63749ad93aa5a4691df0
BRIGHTON-ASUMANI <asumanibrighton@gmail.com>
2018-10-04 15:21:25 +0300

    modified my templates and styling

--- pictures/static/js/main.js
+++ pictures/static/js/main.js
@@ -61,6 +61,8 @@
 
 
 
+
+
 	// Burger Menu
 	var burgerMenu = function() {
 
@@ -188,27 +190,3 @@
 
 
 }());
-
-
-$('#photos').append(allImages);
-
-$(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
-        // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-
-        $('#back-to-top').tooltip('show');
-
-});
